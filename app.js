aaed99d8f13b947ee024c0d7a081b1fff7e9096f
joseenciso <josejefloen@gmail.com>
2021-11-09 23:24:21 +0000

    Project destroyed... Saving in case of making it worse

---
+++ app.js
@@ -0,0 +1,26 @@
+var tabletWidth = 768;
+
+if ( window > tabletWidth) {
+    console.log("No Tablet: " + winWidth);
+
+    $("button .fa-times").addClass('hide');
+    $("button .fa-times").removeClass('show');
+    $("button .fa-hamburger").addClass('hide');
+    $("button .fa-hamburger").removeClass("show");
+
+}
+
+
+function HOLA(){
+    $(window).resize( ()=> {
+        let screenWidth = $(window).width(); 
+
+        console.log("Width " + screenWidth)
+    });
+
+}
+
+$(document).ready( ()=> {
+    HOLA();
+
+});
