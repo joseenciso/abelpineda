b5808652502d7dd0d172cd4e6da43aa2816e79ae
joseenciso <josejefloen@gmail.com>
2021-11-11 22:55:01 +0000

    Nav black background on 75% scrolling JS function

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,25 @@ $(document).ready( ()=> {
     HOLA();
 
 });*/
+
+$(document).ready(function(){
+    
+    let winHeight = $(window).height();
+    console.log(winHeight);
+    $(window).scroll(function(){
+
+        var scroll = $(window).scrollTop();
+        let winHeight = $(window).height();
+        let percentage = winHeight - (winHeight % 75);
+
+        
+        if (scroll > percentage ) {
+            console.log(percentage);
+            $("nav").addClass("black");
+            $("nav").removeClass("nav-top-shawdow");
+        } else {
+            $("nav").removeClass("black");
+            $("nav").addClass("nav-top-shawdow");
+        }
+    })
+  })
