b702c8e8823e5e38871149058c4f32f625363b1d
joseenciso <josejefloen@gmail.com>
2021-11-14 22:51:14 +0000

    Nav container added to ul, burger menu functions according to width minimun to 576 for mobile size

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,6 +27,47 @@ $(document).ready( ()=> {
 
 $(document).ready(function(){
     
+    $(window).resize( ( )=> {
+        let widthWindow = $(window).width();
+        console.log(widthWindow);
+
+        if ( widthWindow < 576 ) {
+            console.log("W: " + widthWindow);
+            let hamburger = $(".fa-hamburger");
+            let times = $(".fa-times");
+            console.log(hamburger);
+            console.log(times);
+        }
+
+    });
+
+    let burgerButton = $(".btn");
+    console.log(burgerButton);
+
+    $("button .fa-hamburger").click( ( ) => {
+        console.log("Hamburger");
+        $(".fa-times").css("display", "block");
+        $(".fa-hamburger").css("display", "none");
+        $("nav .burger-menu").css("top", "0");
+    });
+
+    $("button .fa-times").click( ( ) => {
+        console.log("Times");
+        $(".fa-times").css("display", "none");
+        $(".fa-hamburger").css("display", "block");
+        $("nav .burger-menu").css("top", "-100vh");
+    });
+
+/*
+    function onWindowsWidth(){
+
+        let winWidth = $(window).width();
+        if ( winWidth <= 576 ) {
+
+            console.log(winWidth);
+        }
+    }*/
+
     let winHeight = $(window).height();
     console.log(winHeight);
     $(window).scroll(function(){
@@ -44,5 +85,8 @@ $(document).ready(function(){
             $("nav").removeClass("black");
             $("nav").addClass("nav-top-shawdow");
         }
-    })
-  })
+    });
+
+    
+    //onWindowsWidth();
+});
